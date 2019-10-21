e176e87c0551b712c0c43237edbd5c0fe0bb3d7d
darthvader2 <kvarshith@outlook.com>
2019-10-21 23:21:28 +0530

    edited new index file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './pages/index.css';
 import App from './pages/App';
 import * as serviceWorker from './pages/serviceWorker';
-import PortFolio from './pages/portfolio'
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<PortFolio/>,document.getElementById('portfolio'));
+
 serviceWorker.unregister();
