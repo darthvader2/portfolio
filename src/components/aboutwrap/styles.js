0bb668eec8b8613ad52aa239099668de9bf5c6ad
darthvader2 <kvarshith@outlook.com>
2020-06-27 13:12:13 +0530

    about-wrap conflicted files

---
+++ src/components/aboutwrap/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const AboutWrapCont=styled.div`
+display : flex;
+position:relative;
+margin-left:5%;
+
+
+`;
+
+export const AboutHeading=styled.div`
+font-family:Monument Extended;
+font-size:2.5em;
+color:white;
+.span{
+    color:red;
+}
+
+`;
