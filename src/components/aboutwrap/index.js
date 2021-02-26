f894c1be69cfb693c19c83a51dc1e5c7d502385e
darthvader2 <kvarshith@outlook.com>
2021-02-26 19:02:33 +0100

    about wrap and added resume

--- src/components/aboutwrap/index.js
+++ src/components/aboutwrap/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Container} from '../container/container';
 import {AboutWrapCont,AboutText,AboutHeading,AboutContent} from './styles';
+import cv from '../../documents/Varshith_CV.pdf'
 function AboutWrap(){    
     return(
     
@@ -10,9 +11,12 @@ function AboutWrap(){
             Hi,there
         </AboutHeading>
         <AboutText>
-        Currently pursuing master's in statistics and machine learning  at <br/>
+        <p>I am urrently pursuing master's in statistics and machine learning  at <br/>
         Linkoping university in sweden,even though currently pursuing field of <br/>
-        data analytics ,i like keep myself busy with web technologies always.<br/>
+        data analytics ,I like keep myself busy with web development projects<br/>
+        i posess a couple of skills which can be useful to you ,I didn't list<br/>
+        them here,as they are too boring to be on my portfolio,but if you are<br/>
+        curious you can have a look at my <a href = {cv} target = "_blank">CV</a></p>
         </AboutText>
          
 
