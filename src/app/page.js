12ec7204221ce3ece3e777172b26277ef32f011b
darthvader2 <kvarshith@outlook.com>
2025-08-24 18:06:49 +0200

    newfixes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     
+    <div>
     <h1>Hello, I'm Varshith</h1>
+    <p>I'm devops engineer based in linkoping, Sweden. Currently working as Research engineer at linkoping university.</p>
+    </div>
   )
 }
